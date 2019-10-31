612c56cdc351be0cce91be4b5bb6acbfafd1fa7f
user 0386 <user0386@adsn.work>
2019-10-31 10:56:43 +0200

    transition from gulp3 to gulp4

--- dist/js/main.js
+++
@@ -1,9 +0,0 @@
-/* Start Include libraries, sliders... (jquery, slick, swiper...) */
-
-/* End Include libraries, sliders... (jquery, slick, swiper...) */
-
-/* Start Include components */
-
-/* End Include components */
-"use strict";
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7O0FBRUE7O0FBR0E7O0FBRUEiLCJzb3VyY2VzQ29udGVudCI6WyIvKiBTdGFydCBJbmNsdWRlIGxpYnJhcmllcywgc2xpZGVycy4uLiAoanF1ZXJ5LCBzbGljaywgc3dpcGVyLi4uKSAqL1xuXG4vKiBFbmQgSW5jbHVkZSBsaWJyYXJpZXMsIHNsaWRlcnMuLi4gKGpxdWVyeSwgc2xpY2ssIHN3aXBlci4uLikgKi9cblxuXG4vKiBTdGFydCBJbmNsdWRlIGNvbXBvbmVudHMgKi9cblxuLyogRW5kIEluY2x1ZGUgY29tcG9uZW50cyAqLyJdLCJmaWxlIjoibWFpbi5qcyJ9
